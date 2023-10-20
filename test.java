9ef275a6edb054502800c8fb681fd1c91c26536e
iforce428 <iforce428@gmail.com>
2023-10-20 20:49:30 +0800

    final

--- test.java
+++ test.java
@@ -1,5 +1,26 @@
+abstract class C1{
+    public C1(){
+        System.out.print(1);
+    }
+}
+class solarSystem {
+    int s;
+    void display(){
+        System.out.println(s);
+    }
+}
+
+class Moon extends solarSystem {
+    int m;
+    void display(){
+        System.out.println(m);
+    }
+}
 public class test {
     public static  void main (String[]args) {
-        System.out.println("hello wolrd");
+        Moon moonObj = new Moon();
+        moonObj.s = 10;
+        moonObj.m = 40;
+        moonObj.display();
     }
 }
