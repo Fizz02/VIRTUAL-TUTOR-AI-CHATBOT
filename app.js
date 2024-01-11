0c43dc286fb84ef4e1f0efd1bd08b1602b82ce1a
Fizz02 <114352021+Fizz02@users.noreply.github.com>
2024-01-11 14:15:10 +0800

    fixes : change page router code into OOP-like in app.js

--- app.js
+++ app.js
@@ -7,54 +7,34 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // use res.render to load up an ejs view file
-
 app.use('/assets', express.static('assets'));
-// index page
-app.get('/', function(req, res) {
 
-  res.render('pages/index');
-});
+class PageRouter {
+  constructor(app) {
+    this.app = app;
+    this.setupRoutes();
+  }
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+  setupRoutes() {
+    this.app.get('/', this.renderPage.bind(this, 'index'));
+    this.app.get('/about', this.renderPage.bind(this, 'about'));
+    this.app.get('/calc', this.renderPage.bind(this, 'calculator'));
+    this.app.get('/general', this.renderPage.bind(this, 'algebra_calculator'));
+    this.app.get('/number', this.renderPage.bind(this, 'nomborbulat'));
+    this.app.get('/coord', this.renderPage.bind(this, 'coordinate'));
+    this.app.get('/manage', this.renderPage.bind(this, 'data_management'));
+    this.app.get('/measurement', this.renderPage.bind(this, 'measurement'));
+    this.app.get('/money', this.renderPage.bind(this, 'money'));
+    this.app.get('/space', this.renderPage.bind(this, 'space'));
+    this.app.get('/time', this.renderPage.bind(this, 'time'));
+  }
 
-app.get('/calc', function(req, res) {
-  res.render('pages/calculator');
-});
+  renderPage(pageName, req, res) {
+    res.render(`pages/${pageName}`);
+  }
+}
 
-app.get('/general', function(req,res){
-  res.render('pages/algebra_calculator');
-});
-
-app.get('/number', function(req,res){
-  res.render('pages/nomborbulat');
-});
-
-app.get('/coord', function(req,res){
-  res.render('pages/coordinate');
-});
-
-app.get('/manage', function(req,res){
-  res.render('pages/data_management');
-});
-
-app.get('/measurement', function(req,res){
-  res.render('pages/measurement');
-});
-
-app.get('/money', function(req,res){
-  res.render('pages/money');
-});
-
-app.get('/space', function(req,res){
-  res.render('pages/space');
-});
-
-app.get('/time', function(req,res){
-  res.render('pages/time');
-});
+const pageRouter = new PageRouter(app);
 
 app.listen(8080);
 console.log('Server is listening on port 8080');
